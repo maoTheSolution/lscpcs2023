245eafc4c180a4efef7b52ec5b52be1170fe1070
Minhyuk Adam Oh <minhyukoh@Minhyuks-MacBook-Pro.local>
2023-10-30 09:32:57 +0900

    Week6 in-clas work

---
+++ Week06/Sample01.py
@@ -0,0 +1,29 @@
+# draw color filled circle in turtle 
+  
+import turtle 
+  
+# creating turtle pen 
+t = turtle.Turtle() 
+  
+# taking input for the radius of the circle 
+r = int(input("Enter the radius of the circle: ")) 
+  
+# taking the input for the color 
+col = input("Enter the color name or hex value of color(# RRGGBB): ") 
+  
+# set the fillcolor 
+t.fillcolor(col) 
+  
+# start the filling color 
+t.begin_fill() 
+  
+# drawing the circle of radius r 
+t.circle(r) 
+
+# Event Handlers
+turtle.listen()
+  
+# ending the filling of the color 
+t.end_fill() 
+
+turtle.exitonclick() 
