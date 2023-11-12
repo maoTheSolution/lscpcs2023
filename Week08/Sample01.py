d97c3eb5aa96125652dc0e32a3c6a9c9969513d7
Minhyuk Adam Oh <minhyukoh@Minhyuks-MacBook-Pro.local>
2023-11-13 08:40:42 +0900

    in class work

---
+++ Week08/Sample01.py
@@ -0,0 +1,20 @@
+# You will be able to draw a star with the code below. 
+# Try to draw 10 stars with a random size, color, and position
+
+# import for turtle
+import turtle
+ 
+# Starting a Working Screen
+ws = turtle.Screen()
+ 
+# initializing a turtle instance
+geekyTurtle = turtle.Turtle()
+ 
+# executing loop 5 times for a star
+for i in range(5):
+ 
+        # moving turtle 100 units forward
+        geekyTurtle.forward(100)
+ 
+        # rotating turtle 144 degree right
+        geekyTurtle.right(144)
